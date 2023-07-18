5f0c59c4a87c2fa37add19baec031e5137c17470
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-18 23:14:11 +0530

    MEDIA QUERIES ARE WORKING

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+//import React from "react";
+import React, { Fragment } from 'react';
 import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import Media from 'react-media';
 
 function Header() {
+  // return (
+    
+  //   <div>
+     
+  //   <Media query="(max-width: 599px)" render={() =>
+  //     (
+  //       <p>I am small!</p>
+  //     )}
+  //   />
+  // </div>
+  // );
   return (
-    <header>
-      <h1><DriveFileRenameOutlineIcon/> Keeper</h1>
-    </header>
+    <div>
+      <Media queries={{
+        small: "(max-width: 719px)"
+      }}>
+        {matches => (
+          <Fragment>
+            {matches.small ?
+              <header className=' headerSmall'>
+                <h1 className='h1Small'><DriveFileRenameOutlineIcon/> Keeper</h1>
+              </header> 
+            :
+              <header>
+                <h1><DriveFileRenameOutlineIcon/> Keeper</h1>
+              </header>
+            }
+            
+          </Fragment>
+        )}
+      </Media>
+    </div>
   );
 }
 
