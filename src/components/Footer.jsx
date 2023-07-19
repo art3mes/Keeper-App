ef8784918a764262829646a5a5b1f16d77df7ff6
art3mes <93867313+art3mes@users.noreply.github.com>
2023-07-19 20:59:40 +0530

    backend link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import RefreshIcon from '@mui/icons-material/Refresh';
-//import Fab from '@mui/material/Fab';
 
 function Footer() {
   const year = new Date().getFullYear();
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
   return (
     <div>
         <footer>
           <p>Copyright ⓒ {year}</p>
         </footer>
-        <button className="refreshButton"><RefreshIcon /></button>
+        <button className="refreshButton" onClick={() => openInNewTab('https://keeper-app-backend-dyr1.onrender.com/')}><RefreshIcon /></button>
     </div>
   );
 }
